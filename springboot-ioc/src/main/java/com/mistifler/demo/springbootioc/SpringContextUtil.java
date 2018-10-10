1219081772f13d6018d8daceabfb1e5ed09fdbaa
mistifler <mistifler@126.com>
2018-10-10 23:22:12 +0800

    get ApplicationContext

---
+++ springboot-ioc/src/main/java/com/mistifler/demo/springbootioc/SpringContextUtil.java
@@ -0,0 +1,22 @@
+package com.mistifler.demo.springbootioc;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * Created by mistifler on 2018/10/10.
+ */
+public class SpringContextUtil implements ApplicationContextAware {
+    private static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return context;
+    }
+    
+}
